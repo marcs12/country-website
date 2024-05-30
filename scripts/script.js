c18af400b621dfb6dbb52e5aaaf82382244e311d
Marc Sapa <marcgsapa@gmail.com>
2024-05-29 18:34:35 -0700

    added mobile sidebar for hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,21 @@
 "use strict";
 
-// Hamburger Menu Toggle
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+// create a toggle function for hamburger menu
+const hamNav = document.querySelectorAll(".hamburger-menu");
+const navMenu = document.getElementById("navigation-menu");
+const toggledView = document.getElementById("main-ham");
+
+const closeHam = document.getElementById("close-ham-img");
+
+// create event listener for hamburger menu
+hamNav.forEach((ham) => {
+  ham.addEventListener("click", () => {
+    console.log("clicked");
+    toggledView.classList.toggle("show");
+  });
+});
+
+// create event listener for close button
+closeHam.addEventListener("click", () => {
+  toggledView.classList.toggle("show");
+});
